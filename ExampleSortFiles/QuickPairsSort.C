28696113b77d0c12cac701d1df0f7cb4e9ff5b6e
jsmallcombe <james.smallcombe@outlook.com>
2024-02-24 12:34:47 +0900

    Quick sort code added

--- ExampleSortFiles/QuickPairsSort.C
+++ ExampleSortFiles/QuickPairsSort.C
@@ -1,4 +1,4 @@
-void ExptSort(const char * DetDataFileName,const char * HistFolder,double beta=0,const char * outfileName = "DicePostSort.root", const char *ntuplefileName = "g4out.root") {
+void QuickPairsSort(const char * outfileName = "QuickSortOut.root", const char *ntuplefileName = "g4out.root") {
 	    
 	//// Random number generator to add electronic noise on to "perfect" geant4 detectors ///
 	TRandom r;
@@ -15,54 +15,15 @@ void ExptSort(const char * DetDataFileName,const char * HistFolder,double beta=0
     if(newtree==nullptr)return;
 
 	
-	TFile DetDataFile(DetDataFileName,"READ");
-	gROOT->cd();
-	
-	TGraph* AngleExpt[16][4];
-	if(beta>0){
-		for(int b=0;b<16;b++){
-			for(int m=0;m<4;m++){
-				stringstream ss;
-				ss<<"CorrAngles/grid"<<b<<"_"<<m;
-				TGraph *grf = (TGraph*)DetDataFile.Get(ss.str().c_str());
-				
-				if(grf){
-					AngleExpt[b][m]=grf;
-				}else{
-					cout<<endl<<"NO GRAPH"<<endl;
-					return;
-				}
-			}
-		}
-	}
-	
 	TFile out(outfileName,"UPDATE");
-	out.mkdir(HistFolder);
-	gROOT->cd();
-	
-	
-	TH2 *gdch = (TH2*)DetDataFile.Get("E_GoodChan");
-	if(!gdch){
-		cout<<endl<<"No Good Chan"<<endl;
-		return;
-	}
-	
-	out.cd(HistFolder);//cd into output file so histograms created in memory are associated with that file at creation rather than being manually saved there later.
 
-		TH1F* E_RawSum=new TH1F("Raw","RawHitEnergy;Electron Energy (keV);Counts",2000,0,2000);
-		TH1F* E_AddbackSum=new TH1F("AddbackSum","AddbackSum;Electron Energy (keV);Counts",2000,0,2000);
-		TH1F* E_AddbackVetoSum=new TH1F("AddbackVetoSum","AddbackVetoSum;Electron Energy (keV);Counts",2000,0,2000);
-		TH1F* EGrigGate=new TH1F("EGrigGate","EGrigGate;Electron Energy (keV);Counts",2000,0,2000);
-		TH1F* E_Corr[4];
-		
-		if(beta>0){
-			E_Corr[0]=new TH1F("E_CorrMean","E_CorrMean;Electron Energy (keV);Counts",2000,0,2000);
-			E_Corr[1]=new TH1F("E_CorrMode","E_CorrMode;Electron Energy (keV);Counts",2000,0,2000);
-			E_Corr[2]=new TH1F("E_CorrMeanStrict","E_CorrMeanStrict;Electron Energy (keV);Counts",2000,0,2000);
-			E_Corr[3]=new TH1F("E_CorrModeStrict","E_CorrModeStrict;Electron Energy (keV);Counts",2000,0,2000);
-		}
-	
-	gROOT->cd();//cd back into main session memory 
+	TH1F* E_RawSum=new TH1F("Raw","RawHitEnergy;Electron Energy (keV);Counts",2000,0,2000);
+	TH1F* E_AddbackSum=new TH1F("AddbackSum","AddbackSum;Electron Energy (keV);Counts",2000,0,2000);
+	TH1F* E_AddbackVetoSum=new TH1F("AddbackVetoSum","AddbackVetoSum;Electron Energy (keV);Counts",2000,0,2000);
+	TH1F* E_AddVetoSumMultTwo=new TH1F("E_AddVetoSumMultTwo","E_AddVetoSumMultTwo;Electron Energy (keV);Counts",2000,0,2000);
+	TH1F* E_AVSMTP=new TH1F("E_AVSMTPair","E_AVSMTPair;Electron Energy (keV);Counts",2000,0,2000);
+
+	gROOT->cd();
 	
 	// The TTree newtree is the big list of energies and detector segments we need to load from
 	// First we get the length of the list and output it
@@ -81,9 +42,9 @@ void ExptSort(const char * DetDataFileName,const char * HistFolder,double beta=0
 	newtree->SetBranchAddress("detNumber",&detNumber);
 	newtree->SetBranchAddress("cryNumber",&cryNumber);
 	newtree->SetBranchAddress("depEnergy",&depEnergy);
-	newtree->SetBranchAddress("primaryTheta",&primaryTheta);
-	newtree->SetBranchAddress("posx",&posx);
-	newtree->SetBranchAddress("posz",&posz);
+// 	newtree->SetBranchAddress("primaryTheta",&primaryTheta);
+// 	newtree->SetBranchAddress("posx",&posx);
+// 	newtree->SetBranchAddress("posz",&posz);
 
 	// As each detector within one physical event is stored as a new "line/row" in our list we have to collect them first
 	// Thes variabls to help with assembling the events
@@ -99,7 +60,6 @@ void ExptSort(const char * DetDataFileName,const char * HistFolder,double beta=0
     double Mult=0;
     double TMult=0;
     
-    double pX,pZ;
 
 	// A loop over every line of the input data in sequence
 	for(long jentry=0;jentry<nentries;jentry++){
@@ -126,10 +86,6 @@ void ExptSort(const char * DetDataFileName,const char * HistFolder,double beta=0
                     EventHolder[detNumber]=e;
                     E_RawSum->Fill(e);
                     Mult++;
-                    if(Mult==1){
-                        pX=posx;
-                        pZ=posz;
-                    }
                 }
             }
             Theta=primaryTheta;
@@ -150,48 +106,28 @@ void ExptSort(const char * DetDataFileName,const char * HistFolder,double beta=0
         /////////////////////////
         ////  Now we have collected a complete event and can analyse the event
         ////////////////////////
-		
-        double Esumtmp=0;
-        double Emax=0;
-        int Seg=0;
+
+        int Gmin=16;
+        int Gmax=0;
         for(unsigned int i=0;i<EventHolder.size();i++){
-            double e=EventHolder[i];
-            if(e>30){
-                Esumtmp+=e;
-				if(e>Emax){
-					Emax=e;
-					Seg=i;
-				}
-            }else if(Esumtmp>0){ // So only segments neigbouring will be summed;
-                break;
+            if(EventHolder[i]>30){
+				if(i>Gmax){Gmax=i;}
+				if(i<Gmin){Gmin=i;}
             }
         }
-        if(abs(Esumtmp-Eaddback)>5)Eaddback=0;
-        
+		
 		if(Eaddback>30) {
 
 			E_AddbackSum->Fill(Eaddback);
 			if(!(VETO1||VETO2)){
 				E_AddbackVetoSum->Fill(Eaddback);
-				
-				
-				if(gdch->GetBinContent(gdch->GetXaxis()->FindBin(Eaddback),Seg+1)>0){
-				
-					EGrigGate->Fill(Eaddback);
-				
-					if(beta>0){
-						for(int m=0;m<4;m++){
-							double et=AngleExpt[Seg][m]->Eval(Eaddback);
-		// 					if(m==0)cout<<endl<<Seg<<" "<<Eaddback<<" "<<et;
-							if(et>0){
-								double ecor=((Eaddback+511-beta*cos(et)*sqrt(Eaddback*Eaddback+2*511*Eaddback))/sqrt(1-beta*beta))-511;
-								E_Corr[m]->Fill(ecor);
-							}
-						}
+				if(Mult>1){
+					E_AddVetoSumMultTwo->Fill(Eaddback);
+					if(Gmax-Gmin>1){
+						E_AVSMTP->Fill(Eaddback);
 					}
 				}
 			}    
-
 		}
 			
 		std::fill(EventHolder.begin(), EventHolder.end(), 0);
